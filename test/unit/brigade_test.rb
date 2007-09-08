ad0cd0457a819a089fdc9707dd793d51c23a595c
Scott Becker <becker.scott@gmail.com>
2007-09-08 06:33:22 +0000

    * added additional test for brigade.address method

--- test/unit/brigade_test.rb
+++ test/unit/brigade_test.rb
@@ -7,4 +7,9 @@ class BrigadeTest < Test::Unit::TestCase
     brigade = Brigade.new :city => "Tampa", :state_region => "FL", :country => "USA"
     assert_equal "Tampa FL USA", brigade.location
   end
+
+  def test_should_generate_address_string_from_city_state_postal_code_and_country
+    brigade = Brigade.new :city => "Tampa", :state_region => "FL", :postal_code => "33604", :country => "USA"
+    assert_equal "Tampa FL 33604 USA", brigade.address
+  end
 end
