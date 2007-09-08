25e56839c76ea34fc7b7288beadbecd7140a64cd
Scott Becker <becker.scott@gmail.com>
2007-09-08 06:31:27 +0000

    * small fix to geokit plugin to work with edge rails * added auto geocoding to brigades model * updated fields on "show" view

--- app/models/brigade.rb
+++ app/models/brigade.rb
@@ -1,5 +1,12 @@
 class Brigade < ActiveRecord::Base
+  acts_as_mappable :auto_geocode => {:field => :location, :error_message => 'Could not geocode location'}
+
   def location
     "#{city} #{state_region} #{country}"
+  end
+  
+  # used by 'acts_as_mappable' for geocoding
+  def address
+    "#{city} #{state_region} #{postal_code} #{country}"
   end
 end
