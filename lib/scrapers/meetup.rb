30e7de8d4557efd5f06705774042ca04b1c8b9af
Jason L Perry <jasper@ambethia.com>
2007-09-08 23:45:30 +0000

    scraped groups from meetup.com

--- lib/scrapers/meetup.rb
+++ lib/scrapers/meetup.rb
@@ -7,24 +7,25 @@ brigades = []
 meetups  = Hpricot(open("http://ruby.meetup.com/about/"))/"#groups li.vcard"
 
 meetups.each do |meetup|
-  meetup_uri  = meetup.at("a.url")["href"].split("?").first
+  meetup_uri  = meetup.at("a.url")[:href].split("?").first
   brigades << {
-    :name             => meetup.at(".fn").inner_text,
-    :membership_size  => meetup.at(".note").inner_text.scan(/(\d*) members/).flatten.pop.to_i,
-    :website_url      => meetup_uri,
-    :lat              => meetup.at(".latitude").inner_text.to_f,
-    :lng              => meetup.at(".longitude").inner_text.to_f,
-    :city             => meetup.at(".locality").inner_text,
-    :state_region     => meetup.at(".region") ? meetup.at(".region").inner_text : nil,
-    :country          => meetup.at(".country-name").inner_text,
-    :feeds            => { :ics => { :uri => "#{meetup_uri}/calendar/ical/" } }
+    "name"            => meetup.at(".fn").inner_text,
+    "membership_size" => meetup.at(".note").inner_text.scan(/(\d*) members/).flatten.pop.to_i,
+    "website_url"     => meetup_uri,
+    "lat"             => meetup.at(".latitude").inner_text.to_f,
+    "lng"             => meetup.at(".longitude").inner_text.to_f,
+    "city"            => meetup.at(".locality").inner_text,
+    "state_region"    => meetup.at(".region") ? meetup.at(".region").inner_text : nil,
+    "country"         => meetup.at(".country-name").inner_text,
+    "subdomain"       => meetup.at(".locality").inner_text.downcase.gsub(/[^\w\.\-]/,'_'),
+    "feeds"           => { "ics" => { "uri" => "#{meetup_uri}calendar/ical/" } }
   }
 end
 
 brigades.each do |brigade|
-  meetup = Hpricot open(brigade[:website_url])
-  brigade[:description]    = meetup.at(".groupDesc p") ? meetup.at(".groupDesc p").inner_text : nil
-  brigade[:established_on] = Date.parse(meetup.at("#groupFounded").inner_text.chomp)
+  meetup = Hpricot open(brigade["website_url"])
+  brigade["description"]    = meetup.at(".groupDesc p") ? meetup.at(".groupDesc p").inner_text : nil
+  brigade["established_on"] = Date.parse(meetup.at("#groupFounded").inner_text.chomp)
 end
 
 puts brigades.to_yaml
