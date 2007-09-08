1063bd9a5e3a353003aa3b780aaae4463c0b9983
Scott Becker <becker.scott@gmail.com>
2007-09-08 14:54:23 +0000

    * minor modification to simple_http_auth to get it to work with edge rails. (render is a protected method, had to use "send" to break through) * use simple authentication to block just anyone from accessing logged_exceptions controller

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,5 +17,14 @@ config.action_controller.perform_caching             = true
 # Disable delivery errors, bad email addresses will be ignored
 # config.action_mailer.raise_delivery_errors = false
 
+# require authentication for exception_logger
+config.after_initialize do
+  require 'application' unless Object.const_defined?(:ApplicationController)
+  LoggedExceptionsController.class_eval do
+    requires_authentication :using => Proc.new{ |username, password| password == 'bluefish' },
+                            :realm => 'RubyBrigade!!!!'
+  end
+end
+
 ENV["RECAPTCHA_PUBLIC_KEY"]  = "6LfrXQAAAAAAADTzQtdY-4Z5UJn7bBbjtf35k5Qj"
 ENV["RECAPTCHA_PRIVATE_KEY"] = "6LfrXQAAAAAAAFClQSFjsY0EHDiqkQTK6f8iYex2"
