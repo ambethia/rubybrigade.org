e63ab92f7dc5838e7ca7601fef6ebba5cf86b5fc
Jason L Perry <jasper@ambethia.com>
2007-09-09 21:04:40 +0000

    Error pages and analytics

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,14 @@ module ApplicationHelper
     url = "http://rubybrigade.org"
     url += ":#{request.port}" unless request.port == 80
   end
+  
+  def analytics
+   if RAILS_ENV == "production"
+    return <<-END
+    <script src="http://www.google-analytics.com/urchin.js" type="text/javascript"></script>
+    <script type="text/javascript">_uacct = "UA-2098861-4";urchinTracker();</script>
+    END
+    end
+  end
+  
 end
